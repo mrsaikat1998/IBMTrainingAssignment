66f83a2442004b039dbfecb226cd21c4e33b8451
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-14 12:52:14 +0530

    h

--- ClassTimeAssignment/src/com/nt/model/Customer.java
+++ ClassTimeAssignment/src/com/nt/model/Customer.java
@@ -2,17 +2,16 @@ package com.nt.model;
 
 public class Customer {
 	// instance variable
-	int customerId;
-	String customerName;
+	int customerId = 101;
+	String customerName = "Saikat";
 
-	//display method
-	public  void print() {
+	// display method
+	public void print() {
 		System.out.println("Customer Id :" + customerId + "\n Customer Name : " + customerName);
 	}
+
 	public static void main(String[] args) {
 		Customer customer1 = new Customer();
-		customer1.customerId=101;
-		customer1.customerName="Saikat";
 		customer1.print();
 	}
 }
