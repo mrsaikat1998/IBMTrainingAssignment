3debb55becaf6da0682e0f41a798fdf20b920e13
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-14 12:51:18 +0530

    up

--- ClassTimeAssignment/src/com/nt/model/Customer.java
+++ ClassTimeAssignment/src/com/nt/model/Customer.java
@@ -5,17 +5,14 @@ public class Customer {
 	int customerId;
 	String customerName;
 
-	// constructor
-	public Customer(int customerId, String customerName) {
-		this.customerId = customerId;
-		this.customerName = customerName;
-	}
 	//display method
 	public  void print() {
 		System.out.println("Customer Id :" + customerId + "\n Customer Name : " + customerName);
 	}
 	public static void main(String[] args) {
-		Customer customer1 = new Customer(101, "Saikat");
+		Customer customer1 = new Customer();
+		customer1.customerId=101;
+		customer1.customerName="Saikat";
 		customer1.print();
 	}
 }
