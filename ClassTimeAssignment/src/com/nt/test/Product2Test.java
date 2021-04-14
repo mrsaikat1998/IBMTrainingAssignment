833464973fc29c1c08505375daf2b27acb91c868
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-14 16:10:51 +0530

    new

---
+++ ClassTimeAssignment/src/com/nt/test/Product2Test.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+import com.nt.model.Product2;
+
+public class Product2Test {
+
+	public static void main(String[] args) {
+		Product2 product=new Product2("Laptop", 45000.0, 'A');
+		product.getProductDetails();
+	}
+
+}
