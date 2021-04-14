9fde564e59ef1efdf23266c2f8003d7035f8bb30
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-14 16:44:16 +0530

    j

--- ClassTimeAssignment/src/com/nt/test/Product2Test.java
+++ ClassTimeAssignment/src/com/nt/test/Product2Test.java
@@ -7,6 +7,8 @@ public class Product2Test {
 	public static void main(String[] args) {
 		Product2 product=new Product2("Laptop", 45000.0, 'A');
 		product.getProductDetails();
+		Product2 product1=new Product2("Mobile", 3000.0);
+		product1.getProductDetails();
 	}
 
 }
