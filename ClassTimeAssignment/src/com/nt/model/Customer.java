1630d7597ac4d2bc243ce1628ba007b01bdcb5cf
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-14 12:20:26 +0530

    customer

--- ClassTimeAssignment/src/com/nt/model/Customer.java
+++ ClassTimeAssignment/src/com/nt/model/Customer.java
@@ -2,37 +2,16 @@ package com.nt.model;
 
 public class Customer {
 	// instance variable
-	private int customerId;
-	private String customerName;
+	int customerId;
+	String customerName;
 	
-	public Customer() {}
-
 	// constructor
 	public Customer(int customerId, String customerName) {
 		this.customerId = customerId;
 		this.customerName = customerName;
 	}
-
-	// getter & setter
-	public int getCustomerId() {
-		return customerId;
+	
+	public void print() {
+		System.out.println("Customer Id :"+customerId+"\n Customer Name : "+customerName);
 	}
-
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
-
-	public String getCustomerName() {
-		return customerName;
-	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [customerId=" + customerId + ", customerName=" + customerName + "]";
-	}
-
 }
