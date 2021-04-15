b83a52204d7a6eb7490149981d90073dcc000ab7
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-16 02:23:07 +0530

    C

--- Day3Assignment/src/assignment3/Customer.java
+++ Day3Assignment/src/assignment3/Customer.java
@@ -3,14 +3,16 @@ package assignment3;
 public class Customer {
 	private String customerName;
 	private Address residentialAddress;
+	private Address officialAddress;
 
 	public Customer() {
 
 	}
 
-	public Customer(String customerName, Address residentialAddress) {
+	public Customer(String customerName, Address residentialAddress, Address officialAddress) {
 		this.customerName = customerName;
 		this.residentialAddress = residentialAddress;
+		this.officialAddress = officialAddress;
 	}
 
 	public String getCustomerName() {
@@ -28,9 +30,17 @@ public class Customer {
 	public void setResidentialAddress(Address residentialAddress) {
 		this.residentialAddress = residentialAddress;
 	}
-	
+
+	public Address getOfficialAddress() {
+		return officialAddress;
+	}
+
+	public void setOfficialAddress(Address officialAddress) {
+		this.officialAddress = officialAddress;
+	}
+
 	public String getCustomerDetails() {
-		return "Customer : "+customerName+"\n Residential Address : "+residentialAddress;
+		return "Customer : " + customerName + "\n Residential Address : " + residentialAddress+"\n Official Address : "+officialAddress;
 	}
 
 }
