9ffc4ccee8e6afe65f254e30aaf1b7665820e709
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-16 13:50:46 +0530

    Day4Assignment

---
+++ Day4Assignment/src/assignment1/abstractc/TravelCardTest.java
@@ -0,0 +1,14 @@
+package assignment1.abstractc;
+
+public class TravelCardTest {
+
+	public static void main(String[] args) {
+		TravelCard tc=new TravelCard();
+		tc.setAvailableBalance(100000);
+		tc.setSwipeLimit(3);
+		tc.setCardNo(3319427);
+		
+		tc.swipeCard(20);
+	}
+
+}
