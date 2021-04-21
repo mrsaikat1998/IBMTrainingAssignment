9df68d6e4ac5b5cd4796b8aaab9a35fc0e6934ad
Saikat Mondal <saikatmondal427@gmail.com>
2021-04-21 16:26:12 +0530

    ArrayList Demo ListIterator

---
+++ 10-Day7Assignment/src/com/collection/arraylist/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package com.collection.arraylist;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("Saikat");
+		al.add("Ram");
+		al.add("Shyam");
+		al.add("Pavan");
+		al.add("Allu");
+
+		ListIterator li = al.listIterator();
+		while (li.hasNext()) {
+			String name = (String) li.next();
+			System.out.println(name.toUpperCase());
+		}
+		System.out.println("=============================");
+		while (li.hasPrevious()) {
+			String name = (String) li.previous();
+			System.out.println(name.toUpperCase());
+		}
+
+	}
+
+}
